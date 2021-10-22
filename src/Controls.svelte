2bf7d68253d2ad7057c0e24bea6d8e15d33530fd
Marc Lundgren <marc.lundgren@kareo.com>
2021-10-22 13:58:15 -0700

    trim→clip

--- src/Controls.svelte
+++ src/Controls.svelte
@@ -1,10 +1,10 @@
-<script lang='ts'>
-  import moment from 'moment';
-  import Transcoder from './Transcoder.svelte';
+<script lang="ts">
+  import moment from "moment";
+  import Transcoder from "./Transcoder.svelte";
 
   // const PATTERN = "\d{2}:\d{2}:\d{2}(\.?\d{1,3})?";
   // const pattern = /\d{2}:\d{2}:\d{2}(\.?\d{1,3})?/;
-  const timeFormat = `\\d{2}:\\d{2}:\\d{2}(\\.?\\d{1,3})?`
+  const timeFormat = `\\d{2}:\\d{2}:\\d{2}(\\.?\\d{1,3})?`;
 
   let start: string;
   let end: string;
@@ -16,26 +16,25 @@
   export let onTranscode: (src: string) => void = () => {}; // todo remove
   export let transcodedSrc: string;
 
-  console.log('file', file);
-  console.log('duration', duration);
+  console.log("file", file);
+  console.log("duration", duration);
 
   let endPlaceholder;
   if (duration) {
-    
-    const d = moment.duration(duration, 'seconds');
-    let hours = `0${d.get('hours')}`
+    const d = moment.duration(duration, "seconds");
+    let hours = `0${d.get("hours")}`;
     if (hours.length > 2) {
       hours = hours.slice(-2);
     }
-    let minutes = `0${d.get('minutes')}`;
+    let minutes = `0${d.get("minutes")}`;
     if (minutes.length > 2) {
       minutes = minutes.slice(-2);
     }
-    let seconds = `0${d.get('seconds')}`;
+    let seconds = `0${d.get("seconds")}`;
     if (seconds.length > 2) {
       seconds = seconds.slice(-2);
     }
-    let milliseconds = `00${d.get('milliseconds')}`;
+    let milliseconds = `00${d.get("milliseconds")}`;
     if (milliseconds.length > 3) {
       milliseconds = milliseconds.slice(-3);
     }
@@ -48,33 +47,35 @@
   let transcoding = false;
 
   function _onTranscode(src) {
-    console.log('transcoded src', src)
+    console.log("transcoded src", src);
     transcoding = false;
     onTranscode(src);
   }
   function _onLoadFail(err) {
-    console.error('load fail', err);
+    console.error("load fail", err);
     transcoding = false;
   }
   function _onFetchFileFail(err) {
-    console.error('fetch file fail', err);
+    console.error("fetch file fail", err);
     transcoding = false;
   }
   function _onTranscodeFail(err) {
-    console.error('transcode fail', err);
+    console.error("transcode fail", err);
     transcoding = false;
   }
 </script>
 
-<form on:submit={(event) => {
-  event.preventDefault();
-  transcoding = true;
-}}>
+<form
+  on:submit={(event) => {
+    event.preventDefault();
+    transcoding = true;
+  }}
+>
   <label>
     Start Time:
     <input
       required
-      disabled={disabled}
+      {disabled}
       pattern={timeFormat}
       minlength="8"
       maxlength="12"
@@ -87,27 +88,27 @@
     End Time:
     <input
       required
-      disabled={disabled}
+      {disabled}
       pattern={timeFormat}
       minlength="8"
       maxlength="12"
       placeholder={endPlaceholder}
-      type="text" bind:value={end}
+      type="text"
+      bind:value={end}
     />
   </label>
 
-  <button disabled={disabled || transcoding} type="submit">Trim!</button>
+  <button disabled={disabled || transcoding} type="submit">Clip!</button>
 </form>
 
 {#if transcoding}
   <Transcoder
-    
     onLoadFail={_onLoadFail}
     onFetchFileFail={_onFetchFileFail}
     onTranscodeFail={_onTranscodeFail}
-    file={file}
-    start={start}
-    end={end}
+    {file}
+    {start}
+    {end}
     bind:src={transcodedSrc}
   />
 {/if}
